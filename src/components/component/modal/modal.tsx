2bf027ba91102908c4857c0cde5416eff979a1a4
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-10-23 17:57:01 +0900

    Update modal.tsx

--- src/components/component/modal/modal.tsx
+++ src/components/component/modal/modal.tsx
@@ -1,54 +1,133 @@
-import React, { PropsWithChildren, useEffect, useRef } from "react";
-import UiModal from '@mui/material/Modal';
-//import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
+import React,{useCallback, useEffect, useMemo, useState} from "react";
+import { useGlobalFilter, useSortBy,usePagination, useTable, Row, useRowSelect, TableOptions, TableState } from "react-table";
+import { Search } from "../search/search";
+import { COLUMNS } from "../column/columns";
+import { RootState } from "../../../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { PageSelect } from "../combobox/PageSelect";
+import './table.scss'
+import { TablePageMoveButton } from "../buttons/tablePageMoveButton/tablePageMoveButton";
+import { StopCheckbox } from "../checkbox/stopCheckbox";
+import { Theader } from "./theader";
+import { CellSelectHooks } from "../hooks/selectCheckboxHooks";
+import { Tbody } from "./tbody";
+import { Modal } from "../modal/modal";
+import { cellActions } from "../../createslice/createslices";
 
-interface ModalDefaultType {
-    open:boolean
-    values:any
-    setModalOpen:(value: React.SetStateAction<boolean>) => void
-  }
-////
-  export const Modal = ({open,values,setModalOpen,children}:PropsWithChildren<ModalDefaultType>) =>{   
+type Props = {
+    stopCheck:boolean
+    stopHandleCheckbox:(event: React.ChangeEvent<HTMLInputElement>) => void
+}
 
-    const closeModal = () => {
-      setModalOpen(false);     
-    };   
-
-  const style = {
-    position: 'absolute' as 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 1200, 
-    height:600,   
-    bgcolor: 'background.paper',
-    border: 'none',
+export const Table = ({stopCheck,stopHandleCheckbox}:Props) =>{
+    const columns = useMemo(() => COLUMNS,[])
+    const data = useSelector<RootState,any>(state => state.historylast)
+    const cellDispatch = useDispatch();        
+    const [isOpenModal, setOpenModal] = useState<boolean>(false);
+    const [values,setValues] = useState(Object)
+            
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,        
+        setGlobalFilter,   
+        page,
+        nextPage,
+        previousPage,
+        canNextPage,
+        canPreviousPage,
+        pageOptions,
+        gotoPage,
+        pageCount,
+        setPageSize,
+        state,
+        selectedFlatRows,
+                
+    } = useTable({columns
+        , data,
+        autoResetSelectedRows: false,
+    }, useGlobalFilter,useSortBy,usePagination,useRowSelect,CellSelectHooks,
+    ); 
+          
+    const { pageSize,selectedRowIds } = state 
     
-    boxShadow: 24,
-    p: 4,
-  };
-    const {eq} = values
+    const onClickToggleModal = (e: React.MouseEvent<HTMLTableDataCellElement, MouseEvent>,cell:any)=>{
+        const row = cell?.row
+        const column = cell?.column
+        const values = cell?.row?.values
+        const {eq,eqname,timezone} = values
+        const cellVlaue = {eq,eqname,timezone}
+        if(column?.id != 'selection'){
+            if(!row?.isSelected)  {    
+                setValues(cell?.row?.values)
+                cellDispatch(cellActions.cellValues(cellVlaue))
+                setOpenModal(!isOpenModal);
+            }
+        }
+    } 
 
-    return (    
-        <div>
-          <UiModal
-            open={open}            
-            onClose={closeModal}
-            aria-labelledby="modal-modal-title"
-            aria-describedby="modal-modal-description"
-            >
-            <Box sx={style}>
-              <Typography id="title" variant="h6" component="h2">
-                title
-              </Typography>
-              <Typography id="body" sx={{ mt: 2 }}>
-                {children}
-              </Typography>
-            </Box>
-            </UiModal>
-        </div>
+
+    return (
+        <>
+            <div className="table-pagesize">
+                <div className="clsStopCheckbox">
+                    <StopCheckbox check={stopCheck} HandleCheckbox={stopHandleCheckbox}/>
+                    <button  
+                    className="selectButton"
+                    onClick={()=>{const test = selectedFlatRows.map(
+                        (d) => d.original
+                        )
+                        console.log(selectedFlatRows)                    
+                    }}>
+                        옵션
+                    </button>
+                    <Search onSubmit={setGlobalFilter}/>                       
+                </div>
+                <div className="clsTableControll">
+                    <PageSelect pageSize={pageSize} setPageSize={(e) => setPageSize(e)}/>
+                </div>
+            </div>
+            
+            <div className="table">                       
+                <table {...getTableProps()}>
+
+                    <Theader headerGroups={headerGroups}/>
+                    {isOpenModal && (
+                        <Modal 
+                        open={isOpenModal}
+                            setModalOpen={setOpenModal}>
+                            이곳에 children이 들어갑니다.
+                        </Modal>
+                        )            
+                    }
+                    <Tbody 
+                    BodyProps={getTableBodyProps}
+                    page={page}
+                    prepareRow={prepareRow}
+                    cellClick={onClickToggleModal}
+                    />
+                     
+                </table>
+
+                <TablePageMoveButton 
+                gotoPage={gotoPage} 
+                previousPage={previousPage} 
+                nextPage={nextPage}
+                state={state}
+                pageOptions={pageOptions}
+                pageCount={pageCount}
+                canPreviousPage={canPreviousPage}
+                canNextPage={canNextPage}
+                />                   
+            
+            </div>
+            
+            
+        </> 
     );
+}
 
-  }
+export default Table;
+
