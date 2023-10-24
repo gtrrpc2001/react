3db449244df1d1afdf0c4034cbc30dbf40acbd32
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-10-24 14:11:52 +0900

    Update serverApi.ts

--- src/axios/api/serverApi.ts
+++ src/axios/api/serverApi.ts
@@ -4,7 +4,7 @@ import { historyLast } from '../interface/history_last';
 
 
 const client: Axios = axios.create({
-    baseURL: 'http://121.152.22.85:40081',
+    baseURL: 'https://port-0-nestjs-2rrqq2blmpy5nvs.sel5.cloudtype.app',
     headers: {
       'Content-Type': 'application/json',
     }
