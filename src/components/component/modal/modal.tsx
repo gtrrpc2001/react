f883ed62232427d2c1762862ebfc6a7510c8c4ac
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-10-24 09:44:04 +0900

    Update modal.tsx

--- src/components/component/modal/modal.tsx
+++ src/components/component/modal/modal.tsx
@@ -1,133 +1,122 @@
-import React,{useCallback, useEffect, useMemo, useState} from "react";
-import { useGlobalFilter, useSortBy,usePagination, useTable, Row, useRowSelect, TableOptions, TableState } from "react-table";
-import { Search } from "../search/search";
-import { COLUMNS } from "../column/columns";
+import React, { PropsWithChildren, useEffect, useRef } from "react";
+import UiModal from '@mui/material/Modal';
+//import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import BatteryCharging50Icon from '@mui/icons-material/BatteryCharging50';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import { useSelector } from "react-redux";
 import { RootState } from "../../../store/store";
-import { useDispatch, useSelector } from "react-redux";
-import { PageSelect } from "../combobox/PageSelect";
-import './table.scss'
-import { TablePageMoveButton } from "../buttons/tablePageMoveButton/tablePageMoveButton";
-import { StopCheckbox } from "../checkbox/stopCheckbox";
-import { Theader } from "./theader";
-import { CellSelectHooks } from "../hooks/selectCheckboxHooks";
-import { Tbody } from "./tbody";
-import { Modal } from "../modal/modal";
-import { cellActions } from "../../createslice/createslices";
+import { historyLast } from "../../../axios/interface/history_last";
 
-type Props = {
-    stopCheck:boolean
-    stopHandleCheckbox:(event: React.ChangeEvent<HTMLInputElement>) => void
-}
 
-export const Table = ({stopCheck,stopHandleCheckbox}:Props) =>{
-    const columns = useMemo(() => COLUMNS,[])
-    const data = useSelector<RootState,any>(state => state.historylast)
-    const cellDispatch = useDispatch();        
-    const [isOpenModal, setOpenModal] = useState<boolean>(false);
-    const [values,setValues] = useState(Object)
-            
-    const {
-        getTableProps,
-        getTableBodyProps,
-        headerGroups,
-        rows,
-        prepareRow,        
-        setGlobalFilter,   
-        page,
-        nextPage,
-        previousPage,
-        canNextPage,
-        canPreviousPage,
-        pageOptions,
-        gotoPage,
-        pageCount,
-        setPageSize,
-        state,
-        selectedFlatRows,
-                
-    } = useTable({columns
-        , data,
-        autoResetSelectedRows: false,
-    }, useGlobalFilter,useSortBy,usePagination,useRowSelect,CellSelectHooks,
-    ); 
-          
-    const { pageSize,selectedRowIds } = state 
-    
-    const onClickToggleModal = (e: React.MouseEvent<HTMLTableDataCellElement, MouseEvent>,cell:any)=>{
-        const row = cell?.row
-        const column = cell?.column
-        const values = cell?.row?.values
-        const {eq,eqname,timezone} = values
-        const cellVlaue = {eq,eqname,timezone}
-        if(column?.id != 'selection'){
-            if(!row?.isSelected)  {    
-                setValues(cell?.row?.values)
-                cellDispatch(cellActions.cellValues(cellVlaue))
-                setOpenModal(!isOpenModal);
-            }
-        }
+interface ModalDefaultType {
+    open:boolean
+    setModalOpen:(value: React.SetStateAction<boolean>) => void
+  }
+////
+  export const Modal = ({open,setModalOpen,children}:PropsWithChildren<ModalDefaultType>) =>{   
+    const values = useSelector<RootState,any>(state => state.cellValues)
+    const data:historyLast[] = useSelector<RootState,any>(state => state.historylast)
+
+   const getBpm = ():number => {
+    let bpm:number = 0
+    for (var i = 0 ; i < data?.length; i++){
+      if(data[i].eq == values.eq){
+        bpm = data[i].bpm
+        break;
+      }
     } 
+    return bpm ;
+   }
 
+   
 
-    return (
-        <>
-            <div className="table-pagesize">
-                <div className="clsStopCheckbox">
-                    <StopCheckbox check={stopCheck} HandleCheckbox={stopHandleCheckbox}/>
-                    <button  
-                    className="selectButton"
-                    onClick={()=>{const test = selectedFlatRows.map(
-                        (d) => d.original
-                        )
-                        console.log(selectedFlatRows)                    
-                    }}>
-                        옵션
-                    </button>
-                    <Search onSubmit={setGlobalFilter}/>                       
-                </div>
-                <div className="clsTableControll">
-                    <PageSelect pageSize={pageSize} setPageSize={(e) => setPageSize(e)}/>
-                </div>
-            </div>
-            
-            <div className="table">                       
-                <table {...getTableProps()}>
+    const closeModal = () => {
+      setModalOpen(false);     
+    }; 
 
-                    <Theader headerGroups={headerGroups}/>
-                    {isOpenModal && (
-                        <Modal 
-                        open={isOpenModal}
-                            setModalOpen={setOpenModal}>
-                            이곳에 children이 들어갑니다.
-                        </Modal>
-                        )            
-                    }
-                    <Tbody 
-                    BodyProps={getTableBodyProps}
-                    page={page}
-                    prepareRow={prepareRow}
-                    cellClick={onClickToggleModal}
-                    />
-                     
-                </table>
+  const mainBoxstyle = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 800, 
+    height:800,   
+    bgcolor: 'background.paper',
+    border: 5,
+    spacing:0, 
+    borderRadius:12,   
+    boxShadow: 24,
+    paddingInline:0,
+    paddingBlock:0,
+    display:'absolute',
+  };  
 
-                <TablePageMoveButton 
-                gotoPage={gotoPage} 
-                previousPage={previousPage} 
-                nextPage={nextPage}
-                state={state}
-                pageOptions={pageOptions}
-                pageCount={pageCount}
-                canPreviousPage={canPreviousPage}
-                canNextPage={canNextPage}
-                />                   
-            
-            </div>
-            
-            
-        </> 
+  const headerBox = {
+    display:'flex',
+    alignItems:'end'
+  }
+
+  const logoStyle = {
+    width:130,
+    paddingBlockStart:3,
+    paddingInlineStart:3
+  }
+
+  const topBodyStyle = {
+    display:'flex',
+    height:'17%',
+    alignItems:'center',
+    width:'100%'
+  }
+    
+
+    return (    
+        <div>
+          <UiModal
+            open={open}            
+            onClose={closeModal}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box sx={mainBoxstyle}>
+              <Box sx={headerBox}>
+                <Box sx={{display:'flex',alignItems:'center',position:'relative'}}>                  
+                  <Typography sx={logoStyle} id="title" variant="h6" component="h2">
+                    LOOKHEART
+                  </Typography>
+                </Box>
+                <Box sx={{width:'80%',display:'flex', alignItems:'center',
+                justifyContent: 'end',paddingBlockEnd:1,paddingInlineEnd:3}}> 
+                  <BatteryCharging50Icon sx={{transform:'rotate(90deg)'}}/>                
+                </Box>
+              </Box>                            
+              <Box sx={topBodyStyle}>
+                <Box sx={{display:'flex'}}>                                       
+                    <Box 
+                      sx={{display:'inline-block',width:150,height:70,pb:2,
+                      bgcolor:'white',border:3,borderRadius:5,borderColor:'#5388F7',
+                      verticalAlign:'middle',marginLeft:1,textAlign:'center'}} >
+                        <Typography 
+                          sx={{fontFamily:'-moz-initial',fontSize:24,color:'#5388F7',
+                          fontWeight:'bold',marginBlockStart:3}} 
+                          id="bpm" variant="h5" component="h5">
+                          {getBpm()}
+                        </Typography>
+                    </Box>
+                    <Box sx={{position:'absolute',top:67,left:75,}}>
+                      <FavoriteIcon sx={{color:'#5388F7'}} fontSize="medium"/>
+                      
+                    </Box>
+                </Box>
+                <Typography id="body" sx={{ mt: 2 }}>
+                  {children}
+                </Typography>
+              </Box>
+            </Box>
+            </UiModal>
+        </div>
     );
-}
 
-export default Table;
-
+  }
