804f0ec9ee130475eabd86922a1668157aea4c7b
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-11-02 09:50:39 +0900

    Update func.ts

--- src/func/func.ts
+++ src/func/func.ts
@@ -2,9 +2,11 @@
 export const getTime = (includeTime:boolean,mius:boolean=false,num:number = 0):string => {
   const today = new Date()
   const year = today.getFullYear()
-            const month = mius? (today.getDate() - num == 0 ? today.getMonth() : today.grtDate() + 1) : today.getMonth() + 1
+  const getMonth = today.getMonth()
+  const getDate = today.getDate()
+  const month = mius? (getDate - num == 0 ? getMonth : getMonth + 1) : getMonth + 1
             var monthStr: string = month < 10 ? `0${month}` : month.toString()        
-            const date = mius ? getYesterday(today,num) : today.getDate()    
+            const date = mius ? getYesterday(today,num) : getDate    
             var dateStr: string = date < 10 ? `0${date}` : date.toString()
             if(includeTime){
                 const hour = today.getHours()
@@ -16,28 +18,7 @@ export const getTime = (includeTime:boolean,mius:boolean=false,num:number = 0):s
                 return `${year}-${monthStr}-${dateStr} ${getHour}:${getMinute}:${getSecond}`
             }else{
               return `${year}-${monthStr}-${dateStr}`
-<<<<<<< HEAD
             }      
-=======
-            }     
-    const today = new Date()
-    const year = today.getFullYear()
-              const month = mius? today.getMonth() : today.getMonth() + 1
-              var monthStr: string = month < 10 ? `0${month}` : month.toString()        
-              const date = mius ? getYesterday(today,num) : today.getDate()    
-              var dateStr: string = date < 10 ? `0${date}` : date.toString()
-              if(includeTime){
-                  const hour = today.getHours()
-                  var getHour = hour < 10 ? `0${hour}` : hour.toString()
-                  const minute = today.getMinutes()
-                  var getMinute = minute < 10 ? `0${minute}` : minute.toString()
-                  const second = today.getSeconds()
-                  var getSecond = second < 10 ? `0${second}` : second.toString()
-                  return `${year}-${monthStr}-${dateStr} ${getHour}:${getMinute}:${getSecond}`
-              }else{
-                return `${year}-${monthStr}-${dateStr}`
-              }      
->>>>>>> 280c28f2fec087175777852ea88afaaf534104da
 
 }
 
@@ -49,16 +30,4 @@ return yesterday.getDate()
 export const getHour = ():number => {
 const today = new Date()
 return today?.getHours()
-<<<<<<< HEAD
 }
-=======
-}
-  var yesterday = new Date(today.setDate(today.getDate() - num));
-  return yesterday.getDate()
-}
-
-export const getHour = ():number => {
-  const today = new Date()
-  return today?.getHours()
-}
->>>>>>> 280c28f2fec087175777852ea88afaaf534104da
