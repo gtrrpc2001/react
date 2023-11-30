3c61f463f6a7dc9d47dc1770272d42aae5d22c7e
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-11-30 15:43:49 +0900

    Update graphbody.tsx

--- src/page/graph/graphbody.tsx
+++ src/page/graph/graphbody.tsx
@@ -38,7 +38,7 @@ export const GraphBody = ({names,marginTop}:Props) => {
                         
                         break;
                     case kindButton.cal_step :
-                        result = await getCalStep(id,`${calTime[0]} 23:59:59`,calTime[1],13)
+                        result = await getCalStep(id,time,calTime[1],13)
                         v = result?.map((d)=>{
                             return {step:d.step,distanceKM:d.distanceKM,cal:d.cal,calexe:d.calexe,
                             time:d.writetime
