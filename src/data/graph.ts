ef60d7d5bae4e5e49fb3aec81e3661393e5260ed
gtrrpc2001 <cbdgjs3548@naver.com>
2023-12-08 11:19:15 +0900

    commit again

--- src/data/graph.ts
+++ src/data/graph.ts
@@ -15,3 +15,8 @@ export const getGraphBpmHrvArrData = async (eq:string,nowTime:string,time:string
        const data:number[] = await getEcg(`/mslecg/GraphEcg?eq=${eq}&startDate=${startTime}&endDate=${endTime}`)
        return data;
  }
+
+ export const getTest = async (eq:string,startTime:string,endTime:string):Promise<any[]> => {       
+       const data:any[] = await getEcgTime(`/mslecg/ecgTest?eq=${eq}&startDate=${startTime}&endDate=${endTime}`)
+       return data;
+ }
