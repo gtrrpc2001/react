83d76f374deadc9e526b991e8b9065f6d6521956
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2023-12-15 10:05:43 +0900

    Update loginInput.tsx

--- src/components/component/inputs/login/loginInput.tsx
+++ src/components/component/inputs/login/loginInput.tsx
@@ -17,11 +17,7 @@ export const LoginInput = ({id,className,inputClassName,placeholder,onClick,type
     return (
         <div className={className} onClick={onClick}>
             <input
-<<<<<<< HEAD
-                id={id} 
-=======
                 id={id}
->>>>>>> 76b35c21b27a4125ff75a7a115960fb4b36c3d5a
                 className={inputClassName}
                 type={type}
                 ref={ref}
