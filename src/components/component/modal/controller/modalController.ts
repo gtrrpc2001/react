800c4ef48678e49c2098520e835a0b0c1ebeb118
gtrrpc2001 <79436589+gtrrpc2001@users.noreply.github.com>
2024-01-18 10:04:43 +0900

    Update modalController.ts

--- src/components/component/modal/controller/modalController.ts
+++ src/components/component/modal/controller/modalController.ts
@@ -463,11 +463,7 @@ export const progressBarValue = (settingValue:number,value:number,check = false)
          const percent = (calValue/km * 100) >= 100 ? 100 : calValue/km * 100                                                    
           return percent
       }else{
-<<<<<<< HEAD
-          const percent = (value/settingValue * 100) >= 100 ? 100 : value/settingValue * 100          
-=======
           const percent = (value/settingValue * 100) >= 100 ? 100 : value/settingValue * 100  
->>>>>>> bfc1eabbbcce3c4483c27478be71ccb35e50b2f0
           return percent
       }
   }catch{
