56509b9128aa770c0e8166fb738384bfc1d0fbe2
gtrrpc2001 <cbdgjs3548@naver.com>
2024-05-30 17:26:24 +0900

    web update

--- src/components/component/modal/body/bodyhome/modalHome.tsx
+++ src/components/component/modal/body/bodyhome/modalHome.tsx
@@ -3,7 +3,11 @@ import { ModalTopBody } from "./topbody/modalTopbody";
 import { MiddleBody } from "./middlebody/middleBody";
 import { ModalRealTimeGraph } from "../../../../../page/graph/modalGraph";
 import { BottomBody } from "../../component/bottomBody";
-import { getDayjs, getDecimal, getHeartText } from "../../controller/modalController";
+import {
+  getDayjs,
+  getDecimal,
+  getHeartText,
+} from "../../controller/modalController";
 import { modalValues } from "../../../../../axios/interface/modalvalues";
 import { profileModal } from "../../../../../axios/interface/profileModal";
 import { useSelector } from "react-redux";
@@ -11,55 +15,66 @@ import { RootState } from "../../../../../store/store";
 import { useEffect, useState } from "react";
 import { getOnlyArr } from "../../../../../axios/api/serverApi";
 
-
 type Props = {
-    open:boolean
-    modalList: modalValues
-    values:any
-    getProfile:profileModal
-}
+  open: boolean;
+  modalList: modalValues;
+  values: any;
+  getProfile: profileModal;
+};
 
-export const ModalHome = ({open,modalList,values,getProfile}:Props) => {
-  const getYesterdayArrCount = useSelector<RootState,number>(state => state.yesterdayArrCount)
-  const [todayArr,setTodayArr] = useState(getProfile.arrCnt)
+export const ModalHome = ({ open, modalList, values, getProfile }: Props) => {
+  const getYesterdayArrCount = useSelector<RootState, number>(
+    (state) => state.yesterdayArrCount
+  );
+  const [todayArr, setTodayArr] = useState(getProfile.arrCnt);
   const yesterdayArr = getYesterdayArrCount;
-  const startDate = modalList.writetime?.split(' ')[0]    
-  
-  const endDate = getDayjs(startDate,1,'YYYY-MM-DD','day')
-  // const endDate = dayjs(new Date(startDate)).add(1,'day').tz(tz).format('YYYY-MM-DD')
+  const startDate = modalList.writetime?.split(" ")[0];
 
-  const getArr = async() => {
-    const arrCount = await getOnlyArr(`mslecgarr/arrCount?eq=${values.eq}&startDate=${startDate}&endDate=${endDate}`)
-    setTodayArr(arrCount.arrCnt)    
-  }
+  const endDate = getDayjs(startDate, 1, "YYYY-MM-DD", "day");
 
-  useEffect(()=>{
-    getArr()
-  },[modalList.arrCnt])
-    
-    return (
-            <>
-              
-
-              <ModalTopBody bpm={modalList.bpm} arrCnt={todayArr} prevArrCnt={yesterdayArr} HeartText={getHeartText(modalList.arrCnt)} />
-              
-              <MiddleBody 
-              bpm={modalList.bpm} 
-              arrCnt={todayArr} 
-              sleepTime = {getProfile.sleeptime} 
-              upTime={getProfile.uptime}
-              settingBpm={getProfile.bpm}
-              />              
-              
-              <Box>                
-                <ModalRealTimeGraph open_close={open} bpm={modalList.bpm} eq={values.eq} time={modalList.writetime}/>
-              </Box>
-
-              <BottomBody actCal={modalList.actCal} step={modalList.step} temp={modalList.temp} distance={getDecimal(modalList.distance)}/>
-            
-              
-            </>
-              
-            
+  const getArr = async () => {
+    const arrCount = await getOnlyArr(
+      `mslecgarr/arrCount?eq=${values.eq}&startDate=${startDate}&endDate=${endDate}`
     );
-}
+    setTodayArr(arrCount.arrCnt);
+  };  
+
+  useEffect(() => {
+    getArr();
+  }, [modalList.arrCnt]);
+
+  return (
+    <>
+      <ModalTopBody
+        bpm={modalList.bpm}
+        arrCnt={todayArr}
+        prevArrCnt={yesterdayArr}
+        HeartText={getHeartText(modalList.arrCnt)}
+      />
+
+      <MiddleBody
+        bpm={modalList.bpm}
+        arrCnt={todayArr}
+        sleepTime={getProfile.sleeptime}
+        upTime={getProfile.uptime}
+        settingBpm={getProfile.bpm}
+      />
+
+      <Box>
+        <ModalRealTimeGraph
+          open_close={open}
+          bpm={modalList.bpm}
+          eq={values.eq}
+          time={modalList.writetime}
+        />
+      </Box>
+
+      <BottomBody
+        actCal={modalList.actCal}
+        step={modalList.step}
+        temp={modalList.temp}
+        distance={getDecimal(modalList.distance)}
+      />
+    </>
+  );
+};
