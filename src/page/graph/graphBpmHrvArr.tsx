8f866752a3200f56957f05ab1099ac1f9df5afa8
gtrrpc2001 <cbdgjs3548@gmail.com>
2025-03-06 10:38:08 +0900

    test

--- src/page/graph/graphBpmHrvArr.tsx
+++ src/page/graph/graphBpmHrvArr.tsx
@@ -1,11 +1,11 @@
 import { graphKindButton } from "../../axios/interface/graph";
-import { useCallback, useRef } from "react";
-import ReactECharts from "echarts-for-react";
-import {
-  EcgGraphOption,
-  HrvGraphOption,
-  LivingGraphOption,
-} from "./graphOption";
+// import { useCallback, useRef } from "react";
+// import ReactECharts from "echarts-for-react";
+// import {
+//   EcgGraphOption,
+//   HrvGraphOption,
+//   LivingGraphOption,
+// } from "./graphOption";
 import { Box } from "@mui/material";
 
 type Props = {
@@ -17,67 +17,66 @@ type Props = {
   downloadECG: () => void;
 };
 
-export const Graphs = ({
-  data,
-  kind,
-  id,
-  writetime,
-  kindButtonHandler,
-  downloadECG,
-}: Props) => {
-  const x = useRef<string>("");
+export const Graphs = ({}: // data,
+// kind,
+// id,
+// writetime,
+// kindButtonHandler,
+// downloadECG,
+Props) => {
+  // const x = useRef<string>("");
 
-  const formatXAxis = (tickItem: string, index: number): string => {
-    if (data.length != 0) {
-      if (index == 0 || x.current != tickItem) {
-        x.current = tickItem;
-        return tickItem;
-      } else {
-        return "";
-      }
-    } else {
-      return "";
-    }
-  };
+  // const formatXAxis = (tickItem: string, index: number): string => {
+  //   if (data.length != 0) {
+  //     if (index == 0 || x.current != tickItem) {
+  //       x.current = tickItem;
+  //       return tickItem;
+  //     } else {
+  //       return "";
+  //     }
+  //   } else {
+  //     return "";
+  //   }
+  // };
 
-  const getGraphOption = useCallback(() => {
-    if (kind.ecg) {
-      return EcgGraphOption(
-        kind,
-        data,
-        id && writetime ? true : false,
-        formatXAxis,
-        kindButtonHandler,
-        downloadECG
-      );
-    } else if (kind.cal_step) {
-      return LivingGraphOption(
-        kind,
-        data,
-        id && writetime ? true : false,
-        formatXAxis,
-        kindButtonHandler,
-        downloadECG
-      );
-    } else {
-      return HrvGraphOption(
-        kind,
-        data,
-        id && writetime ? true : false,
-        formatXAxis,
-        kindButtonHandler,
-        downloadECG
-      );
-    }
-  }, [kind, data, formatXAxis, kindButtonHandler]);
+  // const getGraphOption = useCallback(() => {
+  //   if (kind.ecg) {
+  //     return EcgGraphOption(
+  //       kind,
+  //       data,
+  //       id && writetime ? true : false,
+  //       formatXAxis,
+  //       kindButtonHandler,
+  //       downloadECG
+  //     );
+  //   } else if (kind.cal_step) {
+  //     return LivingGraphOption(
+  //       kind,
+  //       data,
+  //       id && writetime ? true : false,
+  //       formatXAxis,
+  //       kindButtonHandler,
+  //       downloadECG
+  //     );
+  //   } else {
+  //     return HrvGraphOption(
+  //       kind,
+  //       data,
+  //       id && writetime ? true : false,
+  //       formatXAxis,
+  //       kindButtonHandler,
+  //       downloadECG
+  //     );
+  //   }
+  // }, [kind, data, formatXAxis, kindButtonHandler]);
 
   return (
     <Box>
-      <ReactECharts
+      {/* <ReactECharts
         notMerge={true}
         lazyUpdate={true}
         option={getGraphOption()}
-      />
+      /> */}
     </Box>
   );
 };
